75a62d0c6bed0c940167711e245530913660bbe2
JackNoordhuis <Jack@CrazedCraftMC.net>
2017-10-26 21:55:10 +1100

    fix bad copy paste

--- src/core/gui/item/defaults/serverselector/ServerSelection.php
+++ src/core/gui/item/defaults/serverselector/ServerSelection.php
@@ -78,12 +78,12 @@ class ServerSelection extends GUIItem {
 		$currentServer = $networkManager->getMap()->getServer();
 		$node = $networkManager->getMap()->findNode($this->node);
 		if($node instanceof NetworkNode) {
-			if(!($server = $node->findServer($this->serverId)) instanceof NetworkServer) {
+			if(!($server = $node->findServer($this->serverId)) instanceof NetworkServer and $this->serverId === self::SERVER_ID_INVALID) {
 				$server = $node->getSuitableServer();
 			}
 
 			if($server instanceof NetworkServer) {
-				if($server->getNetworkId() !== $currentServer->getNetworkId() and $this->serverId === self::SERVER_ID_INVALID) {
+				if($server->getNetworkId() !== $currentServer->getNetworkId()) {
 					if($server->isAvailable()) {
 						return LanguageUtils::translateColors("&7(&d{$server->getOnlinePlayers()}&0/&5{$server->getMaxPlayers()}&7)");
 					} else {
