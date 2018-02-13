928bf78a5999808711c9958cdf419bde259e57d0
JackNoordhuis <daswaggamer@gmail.com>
2018-02-13 20:20:59 +1100

    Add methods to assist with config reading

--- src/core/util/ConfigUtils.php
+++ src/core/util/ConfigUtils.php
@@ -18,12 +18,66 @@ declare(strict_types=1);
 
 namespace core\util;
 
+use core\exception\InvalidConfigException;
+use core\language\LanguageUtils;
+use pocketmine\entity\Effect;
+use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\Item;
 
 class ConfigUtils {
 
-	public static function parseArrayItem(array $data) : ?Item {
+	/**
+	 * Construct an item from an array representation
+	 *
+	 * @param array $itemData
+	 *
+	 * @return null|Item
+	 */
+	public static function parseArrayItem(array $itemData) : ?Item {
+		$item = (is_string($itemData["id"]) ? Item::fromString($itemData["id"]) : Item::get((int) $itemData["id"]));
 
+		$item->setDamage((int) ($itemData["meta"] ?? 0));
+		$item->setCount((int) ($itemData["count"] ?? 1));
+
+		$item->setCustomName(LanguageUtils::translateColors($itemData["name"] ?? ""));
+		foreach($itemData["enchantments"] ?? [] as $enchData) {
+			$item->addEnchantment(self::parseArrayEnchantment($enchData));
+		}
+
+		return $item;
+	}
+
+	/**
+	 * Construct an enchantment from an array representaion
+	 *
+	 * @param array $enchData
+	 *
+	 * @return Enchantment
+	 */
+	public static function parseArrayEnchantment(array $enchData) : Enchantment {
+		$ench = (is_string($enchData["name"]) ? Enchantment::getEnchantmentByName($enchData["name"]) : Enchantment::getEnchantment((int) $enchData["name"]));
+		if(!$ench instanceof Enchantment) throw new InvalidConfigException("Unknown enchantment name supplied for kit item! Value: " . $enchData["name"] ?? "NULL");
+
+		$ench->setLevel($enchData["level"] ?? 1);
+
+		return $ench;
+	}
+
+	/**
+	 * Construct an effect from an array representation
+	 *
+	 * @param array $effectData
+	 *
+	 * @return Effect
+	 */
+	public static function parseArrayEffect(array $effectData) : Effect {
+		$effect = (is_string($effectData["name"]) ? Effect::getEffectByName($effectData["name"]) : Effect::getEffect((int) $effectData["name"]));
+		if(!$effect instanceof Effect) throw new InvalidConfigException("Unknown effect name supplied for kit effect! Value: " . $effectData["name"] ?? "NULL");
+
+		$effect->setDuration((int) ($effectData["time"] ?? 100));
+		$effect->setAmplifier((int) ($effectData["amplifier"] ?? 0));
+
+		return $effect;
 	}
 
 }
